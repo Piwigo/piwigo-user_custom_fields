da7c3da87982b5120d91f8b1d2373f5e1d923a80
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-24 20:43:32 +0100

    Update zh_TW, thanks to closer

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Wording'] = '用字';
+$lang['Admin only'] = '僅限管理者';
+$lang['Are you sure you want to delete the "%s" field?'] = '您確定要刪除 "%s" 欄位嗎？';
+$lang['Create new custom fields'] = '建立新的自定欄位';
+$lang['Custom fields'] = '自定欄位';
+$lang['Custom fields add'] = '增加自定欄位';
+$lang['Custom fields delete'] = '刪除自定欄位';
+$lang['Custom fields manual order was saved'] = '已儲存手動設定的自定欄位順序';
+$lang['Custom fields update'] = '更新自定欄位';
+$lang['Data custom fields update'] = '更新資料自定欄位';
+$lang['Display name'] = '顯示名稱';
+$lang['Edit a custom field'] = '編輯自定欄位';
+$lang['Fields List'] = '欄位列表';
+$lang['Import'] = '匯入';
+$lang['Import info since Add Info Users'] = '使用「新增資訊使用者」複製它的資訊';
+$lang['Import was successful'] = '匯入成功';
+$lang['Manage custom fields'] = '管理自定欄位';
+$lang['Obligatory'] = '必要';
+$lang['Required fields'] = '必要的欄位';
+$lang['To get started, add a new custom field.'] = '請從新增一個新的自定欄位開始';
+$lang['User custom fields'] = '使用者自定欄位';
