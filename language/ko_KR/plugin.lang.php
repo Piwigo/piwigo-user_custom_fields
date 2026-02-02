35771dc092be76bc07bed992f81721b0a48f7655
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-02 11:44:57 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Import info since Add Info Users'] = '사용자 정보 추가를 사용하여 정보를 복사합니다.';
+$lang['Wording'] = '문구';
+$lang['Admin only'] = '관리자 전용';
+$lang['Are you sure you want to delete the "%s" field?'] = '"%s" 필드를 삭제하시겠습니까?';
+$lang['Create new custom fields'] = '새 사용자 정의 필드 만들기';
+$lang['Custom fields'] = '사용자 정의 필드';
+$lang['Custom fields add'] = '사용자 정의 필드 추가';
+$lang['Custom fields delete'] = '사용자 정의 필드 삭제';
+$lang['Custom fields manual order was saved'] = '사용자 정의 필드 수동 순서가 저장되었습니다.';
+$lang['Custom fields update'] = '사용자 정의 필드 업데이트';
+$lang['Data custom fields update'] = '데이터 사용자 정의 필드 업데이트';
+$lang['Display name'] = '표시 이름';
+$lang['Edit a custom field'] = '사용자 정의 필드 편집';
+$lang['Fields List'] = '필드 목록';
+$lang['Import'] = '가져오기';
+$lang['Import was successful'] = '가져오기에 성공했습니다.';
+$lang['Manage custom fields'] = '사용자 정의 필드 관리';
+$lang['Obligatory'] = '필수';
+$lang['Required fields'] = '필수 필드';
+$lang['To get started, add a new custom field.'] = '시작하려면 새 사용자 정의 필드를 추가합니다.';
+$lang['User custom fields'] = '사용자 정의 필드';
