0df1b6ee23752266077c2baaaa2d953be0dd9323
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-06 17:26:54 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Data custom fields update'] = 'Aktualizacja danych pól niestandardowych';
+$lang['Manage custom fields'] = 'Zarządzaj polami niestandardowymi';
+$lang['Obligatory'] = 'Obowiązkowe';
+$lang['Required fields'] = 'Wymagane pola';
+$lang['To get started, add a new custom field.'] = 'Aby rozpocząć, dodaj nowe pole niestandardowe.';
+$lang['User custom fields'] = 'Pola niestandardowe użytkownika';
+$lang['Wording'] = 'Sformułowanie';
+$lang['Import was successful'] = 'Import zakończył się sukcesem';
+$lang['Fields List'] = 'Lista pól';
+$lang['Edit a custom field'] = 'Edytuj pole niestandardowe';
+$lang['Import info since Add Info Users'] = 'Użyj Dodaj informacje o użytkownikach, aby skopiować informacje o nich';
+$lang['Import'] = 'Import';
+$lang['Display name'] = 'Nazwa wyświetlana';
+$lang['Custom fields update'] = 'Uaktualnienie pól niestandardowych';
+$lang['Custom fields manual order was saved'] = 'Ręcznie ustawiona kolejność pól niestandardowych została zapisana';
+$lang['Custom fields delete'] = 'Usuń pola niestandardowe';
+$lang['Custom fields add'] = 'Dodaj pola niestandardowe';
+$lang['Custom fields'] = 'Pola niestandardowe';
+$lang['Admin only'] = 'Tylko administrator';
+$lang['Are you sure you want to delete the "%s" field?'] = 'Czy na pewno chcesz usunąć pole "%s"?';
+$lang['Create new custom fields'] = 'Twórz nowe pola niestandardowe';
